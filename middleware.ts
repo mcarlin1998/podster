8b30c26b8c9cb99b1dd4659fdea942d606421861
Michael Carlin <michaelcarlin076@gmail.com>
2024-06-08 18:47:08 +0100

    Correct routing for optional Catch-All Segments

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 //creates a variable that stores routes that are public to all users
-const isPublicRoute = createRouteMatcher(["sign-in", "/sign-up", "/"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
 
 //If the route isnt in the isPublicRoute array then it needs auth to access it
 export default clerkMiddleware((auth, req) => {
